c92d33f3a37f6e62255cb040e807858cf6a22956
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-17 18:37:27 +0500

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar></NavBar>
         Hello React
       </div>
     );
---
+++ src/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Would You Rather</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/leaderboard">Leader Board</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/add">Add Poll</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
