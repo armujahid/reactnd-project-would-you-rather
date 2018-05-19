14089f3e0025ad791fe9e0d1a61dccf17f9cb16b
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-19 22:44:50 +0500

    improvement: change NotFound component to functional

--- src/components/404Page.js
+++ src/components/404Page.js
@@ -1,15 +1,13 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom'
 
-class NotFound extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>404 Not Found</div>
-        <NavLink to="/">Click here </NavLink> to go back to home page
-      </Fragment>
-    );
-  }
+function NotFound() {
+  return (
+    <Fragment>
+      <div>404 Not Found</div>
+      <NavLink to="/">Click here </NavLink> to go back to home page
+    </Fragment>
+  );
 }
 
 export default NotFound
