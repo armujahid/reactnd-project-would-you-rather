ab70421a5916b4497d564d9cff53d973df5d1c7c
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-19 15:39:57 +0500

    feat: integrate redux-devtools-extension'

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,9 @@
 import thunk from 'redux-thunk'
 import logger from './logger'
 import { applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export default applyMiddleware(
+export default composeWithDevTools(applyMiddleware(
   thunk,
   logger,
-)
+))
