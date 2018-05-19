778c265f46745fabafa1f497d674234344d5363e
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-19 14:11:20 +0500

    feat: add ADD_USER_ANSWER in users action and reducer

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,18 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const ADD_USER_ANSWER = 'ADD_USER_ANSWER'
 
 export function receiveUsers (users) {
   return {
     type: RECEIVE_USERS,
     users,
+  }
+}
+
+export function addUserAnswer (authedUser, qid, answer) {
+  return {
+    type: ADD_USER_ANSWER,
+    authedUser,
+    qid,
+    answer
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,22 @@
-import { RECEIVE_USERS } from '../actions/users'
+import { RECEIVE_USERS, ADD_USER_ANSWER } from '../actions/users'
 
-export default function users (state = {}, action) {
+export default function state (state = {}, action) {
   switch(action.type) {
     case RECEIVE_USERS :
       return {
         ...state,
         ...action.users
+      }
+    case ADD_USER_ANSWER :
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          answers: {
+            ...state[action.authedUser].answers,
+            [action.qid]: action.answer
+          }
+        }
       }
     default :
       return state
