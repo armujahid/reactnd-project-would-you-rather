9a8f30656c0fd87dd1543b2e24c71f773b74054a
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-19 17:27:28 +0500

    feat: hook login logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { setAuthedUser } from '../actions/authedUser'
 
-export default class Login extends React.Component {
+class Login extends Component {
+  static propTypes = {
+    users: PropTypes.object.isRequired,
+    setAuthedUser: PropTypes.func.isRequired
+  }
+  state = {
+    userId : ''
+  }
+
+  onUserChange = (userId) => { this.setState({ userId })}
+
+  onLogin = () => {
+    const { userId } = this.state;
+    const { setAuthedUser } = this.props;
+    if (userId) {
+      setAuthedUser(userId);
+    }
+  }
+
   render() {
+    const { users } = this.props;
     return (
       <Form>
         <FormGroup>
           <Label for="userSelect">Select User</Label>
-          <Input type="select" name="select" id="userSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+          <Input type="select" name="select" onChange={(event) => this.onUserChange(event.target.value)}>
+          {
+            Object.keys(users).map(user =>
+            <option key={user} value={user}>
+              {users[user].name}
+            </option>)
+          }
           </Input>
         </FormGroup>
-        <Button>Login</Button>
+        <Button onClick={this.onLogin}>Login</Button>
       </Form>
     );
   }
 }
+
+function mapStateToProps ({ users }) {
+  return {
+    users
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setAuthedUser: (id) => {
+      dispatch(setAuthedUser(id))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
