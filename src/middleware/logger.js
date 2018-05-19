0a92b6786f9bbd697cdc39e7bcc36dbbc624d23a
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-19 12:30:39 +0500

    feat: add middleware from chirper app

---
+++ src/middleware/logger.js
@@ -0,0 +1,11 @@
+/* eslint no-console: "off" */
+const logger = (store) => (next) => (action) => {
+  console.group(action.type)
+    console.log('The action: ', action)
+    const returnValue = next(action)
+    console.log('The new state: ', store.getState())
+  console.groupEnd()
+  return returnValue
+}
+
+export default logger
