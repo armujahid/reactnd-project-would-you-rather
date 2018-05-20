d81419c2b916dab2daeb7fb2291bb05bf3f5ef1a
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-20 12:47:11 +0500

    feat: redirect to poll details page on clicking a poll

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Card, CardBody, CardTitle} from 'reactstrap';
+import { withRouter } from 'react-router-dom'
 
 const Poll = (props) => {
+  const loadDetails = (e ,id) => {
+    e.preventDefault()
+    props.history.push(`/questions/${id}`)
+  }
   const { poll } = props;
   return (
-    <Card>
+    <Card onClick={(e) => loadDetails(e, poll.id)}>
       <CardBody>
         <CardTitle>Would You Rather</CardTitle>
         <ul>
@@ -20,7 +25,8 @@ const Poll = (props) => {
 
 Poll.propTypes = {
   id: PropTypes.string.isRequired,
-  poll: PropTypes.object.isRequired
+  poll: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 }
 
 function mapStateToProps ({ polls }, { id }) {
@@ -29,4 +35,4 @@ function mapStateToProps ({ polls }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Poll)
+export default withRouter(connect(mapStateToProps)(Poll))
--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Redirect } from 'react-router-dom'
 
-class PollDetails extends Component {
+class PollDetails extends PureComponent {
+  static propTypes = {
+    poll: PropTypes.object
+  }
+
   render() {
+    const { poll } = this.props
+
+    if (!poll) {
+      return <Redirect to='/404' />
+    }
+
     return (
       <div>Poll Details Page</div>
     );
   }
 }
 
-export default PollDetails
+function mapStateToProps ({ polls }, props) {
+  const { id } = props.match.params
+  const poll = polls[id]
+  return {
+    poll
+  }
+}
+
+export default connect(mapStateToProps)(PollDetails)
