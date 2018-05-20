a7c2e7c2f1c2d04113577951dd2a4ad66420a366
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-05-20 21:21:52 +0500

    feat: add leaderboard functionality

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { Table } from 'reactstrap'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import User from './User'
 
-class LeaderBoard extends Component {
+class LeaderBoard extends PureComponent {
+  static propTypes = {
+    table: PropTypes.array.isRequired
+  }
+
   render() {
+    const { table } = this.props
     return (
-      <div>LeaderBoard Page</div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>User</th>
+            <th>Poll Created</th>
+            <th>Poll Answered</th>
+          </tr>
+        </thead>
+        <tbody>
+          { table.map((user, index) =>
+            <tr key={user.id}>
+              <th scope="row">{index + 1}</th>
+              <td><User id={user.id}/></td>
+              <td>{user.created}</td>
+              <td>{user.answered}</td>
+            </tr>)
+          }
+        </tbody>
+      </Table>
     );
   }
 }
 
-export default LeaderBoard
+function mapStateToProps ({ users }) {
+  const table = Object.keys(users).map(id => ({
+    id,
+    created : users[id].questions.length,
+    answered: Object.keys(users[id].answers).length
+
+  })).sort((a, b) =>  b.created + b.answered - (a.created + a.answered))
+  return {
+    table
+  }
+}
+
+export default connect(mapStateToProps)(LeaderBoard)
